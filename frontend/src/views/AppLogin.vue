0f98dd4bd9c4279ec3dc3931116accd43eacd4d0
k3n Bartolome <padillakryss@gmail.com>
2023-01-16 17:54:21 +0800

    message

--- frontend/src/views/AppLogin.vue
+++ frontend/src/views/AppLogin.vue
@@ -2,19 +2,25 @@
   <div class="flex items-center justify-center min-h-full px-4 pb-64 sm:px-6 lg:px-8">
     <div class="w-full max-w-md space-y-8">
       <div class=" items center">
-        <img class="w-auto h-12 mx-auto" src="https://cdn.contactcenterworld.com/images/company/vxi-global-solutions-1200px-logo.png" alt="Your Company" />
+        <img class="w-auto h-12 mx-auto"
+          src="https://cdn.contactcenterworld.com/images/company/vxi-global-solutions-1200px-logo.png"
+          alt="Your Company" />
         <h2 class="mt-6 text-3xl font-bold tracking-tight text-center text-gray-900">Sign in</h2>
-        
+
       </div>
       <form class="mt-8 space-y-6" @submit.prevent="login">
         <div class="-space-y-px rounded-md shadow-sm">
           <div class="flex items center">
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" v-model="email" type="email" autocomplete="email" required="true" class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:z-10 focus:border-orange-600 focus:outline-none focus:ring-orange-600 sm:text-sm" placeholder="Email address" />
+            <input id="email-address" v-model="email" type="email" autocomplete="email" required="true"
+              class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:z-10 focus:border-orange-600 focus:outline-none focus:ring-orange-600 sm:text-sm"
+              placeholder="Email address" />
           </div>
           <div class="flex items center">
             <label for="password" class="sr-only">Password</label>
-            <input id="password" v-model="password" type="password" autocomplete="current-password" required="true" class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:z-10 focus:border-orange-600 focus:outline-none focus:ring-orange-600 sm:text-sm" placeholder="Password" />
+            <input id="password" v-model="password" type="password" autocomplete="current-password" required="true"
+              class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:z-10 focus:border-orange-600 focus:outline-none focus:ring-orange-600 sm:text-sm"
+              placeholder="Password" />
           </div>
         </div>
 
@@ -25,10 +31,8 @@
         </div>
 
         <div class="flex items center">
-          <button type="submit" class="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-orange-600 border border-transparent rounded-md group hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
-            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-              <LockClosedIcon class="w-5 h-5 text-white group-hover:text-orange-600" aria-hidden="true" />
-            </span>
+          <button type="submit"
+            class="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-orange-600 border border-transparent rounded-md group hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
             Sign in
           </button>
         </div>
@@ -36,7 +40,7 @@
     </div>
   </div>
 </template>
-<script>  
+<script>
 import axios from "axios";
 
 export default {
@@ -56,7 +60,7 @@ export default {
         .then((response) => {
           localStorage.setItem("token", response.data.access_token);
           localStorage.setItem("role", response.data.access_role);
-          this.$router.push("/dashboard");
+          this.$router.push({ path: '/dashboard' });
         })
         .catch((error) => {
           console.log(error);
