805deea6849fcf33e3fa2fcfd0fdb5b1db443291
k3n Bartolome <padillakryss@gmail.com>
2023-03-06 00:00:33 -0800

    message

--- frontend/src/views/DashboardNavItems/User/Capfile/ClarkTable.vue
+++ frontend/src/views/DashboardNavItems/User/Capfile/ClarkTable.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/valid-v-for -->
 <template>
   <thead>
     <tr class="font-semibold text-black bg-white border-2 border-gray-400 border-solid">
@@ -74,17 +73,20 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">AT&T BB MULTI SERVICE</td>
-    <td class="px-1 py-2 border" v-for="classA1 in clark1" :key="clark1.id">{{ classA1.total_target }}</td></tbody>
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">AT&T BB MULTI SERVICE</td>
+    <td class="px-1 py-2 border border" v-for="clark1 in clark1" :key="clark1.id">
+      <button>{{ clark1.total_target }}</button>
+    </td>
+  </tbody>
   <tbody
     v-for="(clark2, weeksStart) in groupedData2"
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">EBAY BUY PHONE</td>
-    <td class="px-1 py-2 border" v-for="clark2 in clark2" :key="clark2.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">EBAY BUY PHONE</td>
+    <td class="px-1 py-2 border border" v-for="clark2 in clark2" :key="clark2.id">
       {{ clark2.total_target }}
     </td>
   </tbody>
@@ -93,9 +95,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">EBAY BUY CHAT</td>
-    <td class="px-1 py-2 border" v-for="clark3 in clark3" :key="clark3.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">EBAY BUY CHAT</td>
+    <td class="px-1 py-2 border border" v-for="clark3 in clark3" :key="clark3.id">
       {{ clark3.total_target }}
     </td>
   </tbody>
@@ -104,9 +106,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">EBAY SELL PHONE</td>
-    <td class="px-1 py-2 border" v-for="clark4 in clark4" :key="clark4.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">EBAY SELL PHONE</td>
+    <td class="px-1 py-2 border border" v-for="clark4 in clark4" :key="clark4.id">
       {{ clark4.total_target }}
     </td>
   </tbody>
@@ -115,9 +117,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">EBAY SELL CHAT</td>
-    <td class="px-1 py-2 border" v-for="clark5 in clark5" :key="clark5.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">EBAY SELL CHAT</td>
+    <td class="px-1 py-2 border border" v-for="clark5 in clark5" :key="clark5.id">
       {{ clark5.total_target }}
     </td>
   </tbody>
@@ -126,9 +128,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">ZINUS</td>
-    <td class="px-1 py-2 border" v-for="clark6 in clark6" :key="clark6.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">ZINUS</td>
+    <td class="px-1 py-2 border border" v-for="clark6 in clark6" :key="clark6.id">
       {{ clark6.total_target }}
     </td>
   </tbody>
@@ -137,9 +139,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">MODIVCARE FULL TIME</td>
-    <td class="px-1 py-2 border" v-for="clark7 in clark7" :key="clark7.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">MODIVCARE FULL TIME</td>
+    <td class="px-1 py-2 border border" v-for="clark7 in clark7" :key="clark7.id">
       {{ clark7.total_target }}
     </td>
   </tbody>
@@ -148,9 +150,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">COMCAST SPECIAL PROJECT</td>
-    <td class="px-1 py-2 border" v-for="clark8 in clark8" :key="clark8.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">COMCAST SPECIAL PROJECT</td>
+    <td class="px-1 py-2 border border" v-for="clark8 in clark8" :key="clark8.id">
       {{ clark8.total_target }}
     </td>
   </tbody>
@@ -159,9 +161,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">LUMEN/CRIS PHQ1</td>
-    <td class="px-1 py-2 border" v-for="clark9 in clark9" :key="clark9.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">LUMEN/CRIS PHQ1</td>
+    <td class="px-1 py-2 border border" v-for="clark9 in clark9" :key="clark9.id">
       {{ clark9.total_target }}
     </td>
   </tbody>
@@ -170,9 +172,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">ENS Q1</td>
-    <td class="px-1 py-2 border" v-for="clark10 in clark10" :key="clark10.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">ENS Q1</td>
+    <td class="px-1 py-2 border border" v-for="clark10 in clark10" :key="clark10.id">
       {{ clark10.total_target }}
     </td>
   </tbody>
@@ -181,9 +183,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">CRIS Q2A</td>
-    <td class="px-1 py-2 border" v-for="clark11 in clark11" :key="clark11.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">CRIS Q2A</td>
+    <td class="px-1 py-2 border border" v-for="clark11 in clark11" :key="clark11.id">
       {{ clark11.total_target }}
     </td>
   </tbody>
@@ -192,9 +194,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">CRIS Q2B</td>
-    <td class="px-1 py-2 border" v-for="clark12 in clark12" :key="clark12.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">CRIS Q2B</td>
+    <td class="px-1 py-2 border border" v-for="clark12 in clark12" :key="clark12.id">
       {{ clark12.total_target }}
     </td>
   </tbody>
@@ -203,9 +205,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">FIS</td>
-    <td class="px-1 py-2 border" v-for="clark13 in clark13" :key="clark13.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">FIS</td>
+    <td class="px-1 py-2 border border" v-for="clark13 in clark13" :key="clark13.id">
       {{ clark13.total_target }}
     </td>
   </tbody>
@@ -214,9 +216,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">BRIGHTSPEED T1</td>
-    <td class="px-1 py-2 border" v-for="clark14 in clark14" :key="clark14.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">BRIGHTSPEED T1</td>
+    <td class="px-1 py-2 border border" v-for="clark14 in clark14" :key="clark14.id">
       {{ clark14.total_target }}
     </td>
   </tbody>
@@ -225,9 +227,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">BRIGHTSPEED 2A</td>
-    <td class="px-1 py-2 border" v-for="clark15 in clark15" :key="clark15.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">BRIGHTSPEED 2A</td>
+    <td class="px-1 py-2 border border" v-for="clark15 in clark15" :key="clark15.id">
       {{ clark15.total_target }}
     </td>
   </tbody>
@@ -236,9 +238,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">BRIGHTSPEED 2A</td>
-    <td class="px-1 py-2 border" v-for="clark16 in clark16" :key="clark16.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">BRIGHTSPEED 2A</td>
+    <td class="px-1 py-2 border border" v-for="clark16 in clark16" :key="clark16.id">
       {{ clark16.total_target }}
     </td>
   </tbody>
@@ -247,9 +249,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">COMCAST XM T1</td>
-    <td class="px-1 py-2 border" v-for="clark17 in clark17" :key="clark17.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">COMCAST XM T1</td>
+    <td class="px-1 py-2 border border" v-for="clark17 in clark17" :key="clark17.id">
       {{ clark17.total_target }}
     </td>
   </tbody>
@@ -258,9 +260,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">COMCAST NED 3P</td>
-    <td class="px-1 py-2 border" v-for="clark18 in clark18" :key="clark18.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">COMCAST NED 3P</td>
+    <td class="px-1 py-2 border border" v-for="clark18 in clark18" :key="clark18.id">
       {{ clark18.total_target }}
     </td>
   </tbody>
@@ -269,9 +271,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">COMCAST NED BILLING</td>
-    <td class="px-1 py-2 border" v-for="clark19 in clark19" :key="clark19.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">COMCAST NED BILLING</td>
+    <td class="px-1 py-2 border border" v-for="clark19 in clark19" :key="clark19.id">
       {{ clark19.total_target }}
     </td>
   </tbody>
@@ -280,9 +282,9 @@
     :key="weeksStart"
     class="font-semibold text-black bg-white border-2 border-gray-400 border-solid"
   >
-    <td class="px-1 py-2 border">CLARK</td>
-    <td class="px-1 py-2 border">COMCAST DIGITAL CARE</td>
-    <td class="px-1 py-2 border" v-for="clark20 in clark20" :key="clark20.id">
+    <td class="px-1 py-2 border border">CLARK</td>
+    <td class="px-1 py-2 border border">COMCAST DIGITAL CARE</td>
+    <td class="px-1 py-2 border border" v-for="clark20 in clark20" :key="clark20.id">
       {{ clark20.total_target }}
     </td>
   </tbody>
