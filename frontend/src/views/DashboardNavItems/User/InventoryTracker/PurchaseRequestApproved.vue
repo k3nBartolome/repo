ac77b338f74a249799ea241d4419bd0f8f234aef
Kryss Bartolome (PH) <krbartolome@vxi.com.ph>
2023-08-08 14:14:06 +0800

    message

--- frontend/src/views/DashboardNavItems/User/InventoryTracker/PurchaseRequestApproved.vue
+++ frontend/src/views/DashboardNavItems/User/InventoryTracker/PurchaseRequestApproved.vue
@@ -161,7 +161,7 @@ export default {
       };
 
       axios
-        .put(`http://127.0.0.1:8000/api/inventory/approved/${id}`, form, config)
+        .put(`http://10.109.2.112:8081/api/inventory/approved/${id}`, form, config)
         .then((response) => {
           console.log(response.data.data);
           this.getInventory();
@@ -183,7 +183,7 @@ export default {
       };
 
       axios
-        .put(`http://127.0.0.1:8000/api/inventory/denied/${id}`, form, config)
+        .put(`http://10.109.2.112:8081/api/inventory/denied/${id}`, form, config)
         .then((response) => {
           console.log(response.data.data);
           this.getInventory();
@@ -196,7 +196,7 @@ export default {
     async getInventory() {
       try {
         const token = this.$store.state.token;
-        const response = await axios.get("http://127.0.0.1:8000/api/inventory/denied", {
+        const response = await axios.get("http://10.109.2.112:8081/api/inventory/denied", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -215,7 +215,7 @@ export default {
     async getSites() {
       try {
         const token = this.$store.state.token;
-        const response = await axios.get("http://127.0.0.1:8000/api/sites", {
+        const response = await axios.get("http://10.109.2.112:8081/api/sites", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
