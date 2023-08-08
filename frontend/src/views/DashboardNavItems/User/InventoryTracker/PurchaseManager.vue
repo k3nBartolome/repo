e5ecd8542a1355ad5511d9c5cae4c2204556aa0f
Kryss Bartolome (PH) <krbartolome@vxi.com.ph>
2023-08-08 14:11:21 +0800

    message

--- frontend/src/views/DashboardNavItems/User/InventoryTracker/PurchaseManager.vue
+++ frontend/src/views/DashboardNavItems/User/InventoryTracker/PurchaseManager.vue
@@ -26,213 +26,29 @@
     </div>
   </main>
 </template>
-    <script>
-    import axios from "axios";
-    import DataTable from "datatables.net-vue3";
-    import DataTableLib from "datatables.net-bs5";
-    // eslint-disable-next-line no-unused-vars
-    import Buttons from "datatables.net-buttons-bs5";
-    import ButtonsHtml5 from "datatables.net-buttons/js/buttons.html5";
-    // eslint-disable-next-line no-unused-vars
-    import print from "datatables.net-buttons/js/buttons.print";
-    //import pdfmake from "pdfmake";
-    // eslint-disable-next-line no-unused-vars
-    import pdfFonts from "pdfmake/build/vfs_fonts";
-    import "datatables.net-responsive-bs5";
-    // eslint-disable-next-line no-unused-vars
+<script>
+// computed: {
+//     isAdmin() {
+//       const userRole = this.$store.state.role;
+//       return userRole === "admin" || userRole === "user";
+//     },
+//     isUser() {
+//       const userRole = this.$store.state.role;
+//       return userRole === "user";
+//     },
+//   },
+</script>
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+  .selected-tab {
+    border-color: #6366f1;
+    color: #6366f1;
+    font-weight: bold;
+  }
 
-    import "bootstrap/dist/css/bootstrap.css";
-
-    DataTable.use(DataTableLib);
-    //DataTable.use(pdfmake);
-    DataTable.use(ButtonsHtml5);
-
-    export default {
-      components: { DataTable },
-      data() {
-        return {
-          sites: [],
-          items: [],
-          sites_selected: "",
-          item_name: "",
-          quantity: "",
-          budget_code: "",
-          type: "Non-Food",
-          category: "Normal",
-          date_expiry: "",
-          showModal: false,
-          columns: [
-            { data: "id", title: "ID" },
-            { data: "site.name", title: "Site" },
-            { data: "item_name", title: "Item" },
-            { data: "quantity", title: "Quantity" },
-            { data: "budget_code", title: "Budget Code" },
-            { data: "type", title: "Type" },
-            { data: "category", title: "Category" },
-            { data: "date_expiry", title: "Expiration Date" },
-            { data: "created_at", title: "Added Date" },
-            { data: "created_by.name", title: "Added By" },
-          ],
-        };
-      },
-      computed: {
-
-      },
-      mounted() {
-        window.vm = this;
-        this.getSites();
-        this.getItems();
-
-      },
-      methods: {
-        async getItems() {
-          try {
-            const token = this.$store.state.token;
-            const response = await axios.get("http://10.109.2.112:8081/api/items", {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            });
-
-            if (response.status === 200) {
-              this.items = response.data.items;
-              console.log(response.data.items);
-            } else {
-              console.log("Error fetching items");
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        },
-        async getSites() {
-          try {
-            const token = this.$store.state.token;
-            const response = await axios.get("http://10.109.2.112:8081/api/sites", {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            });
-
-            if (response.status === 200) {
-              this.sites = response.data.data;
-              console.log(response.data.data);
-            } else {
-              console.log("Error fetching sites");
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        },
-        addItems() {
-  const formData = {
-    item_name: this.item_name,
-    quantity: this.quantity,
-    type: this.type,
-    category: this.category,
-    budget_code: this.budget_code,
-    date_expiry: this.date_expiry,
-    site_id: this.sites_selected,
-    is_active: 1,
-    created_by: this.$store.state.user_id,
-  };
-  axios
-    .post("http://10.109.2.112:8081/api/items", formData, {
-      headers: {
-        Authorization: `Bearer ${this.$store.state.token}`,
-      },
-    })
-    .then((response) => {
-      console.log(response.data);
-      this.item_name = "";
-      this.quantity = "";
-      this.sites_selected = "";
-      this.type = "";
-      this.category = "";
-      this.budget_code = "";
-      this.date_expiry = "";
-    })
-    .catch((error) => {
-      console.log(error.response.data);
-    });
-}
-      },
-    };
-    </script>
-    <style>
-    .table-responsive {
-      overflow: auto;
-    }
-
-    .datatable-container {
-      width: 100%;
-    }
-
-    .table {
-      white-space: nowrap;
-    }
-
-    .table thead th {
-      padding: 8px;
-    }
-
-    .table tbody td {
-      padding: 8px;
-    }
-    .modal {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.5);
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-
-    .modal-content {
-      background-color: #fff;
-      padding: 20px;
-      border-radius: 8px;
-      max-width: 400px;
-    }
-input[type="radio"] {
-  appearance: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  width: 16px;
-  height: 16px;
-  border: 2px solid #ccc;
-  border-radius: 50%;
-  outline: none;
-  margin-right: 8px;
-  cursor: pointer;
-  position: relative;
-}
-
-input[type="radio"]:checked {
-  border-color: #3b71ca;
-}
-
-input[type="radio"]:checked::before {
-  content: "";
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 8px;
-  height: 8px;
-  border-radius: 50%;
-  background-color: #3b71ca; /
-}
-input[type="radio"] {
-  width: 20px;
-  height: 20px;
-}
-
-input[type="radio"]:checked::before {
-  width: 10px;
-  height: 10px;
-}
-
-    </style>
+</style>
+    
