e0996fd086d246145d9856a4017142dd999c4f9e
Kryss Bartolome (PH) <krbartolome@vxi.com.ph>
2023-08-30 14:39:41 +0800

    message

--- frontend/src/views/DashboardNavItems/User/InventoryTracker/DashboardAwarded.vue
+++ frontend/src/views/DashboardNavItems/User/InventoryTracker/DashboardAwarded.vue
@@ -290,7 +290,7 @@ export default {
     async getSites() {
       try {
         const token = this.$store.state.token;
-        const response = await axios.get("http://127.0.0.1:8000/api/sites", {
+        const response = await axios.get("http://10.109.2.112:8081/api/sites", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -309,7 +309,7 @@ export default {
     async getAward() {
       try {
         const token = this.$store.state.token;
-        const response = await axios.get("http://127.0.0.1:8000/api/awarded/both", {
+        const response = await axios.get("http://10.109.2.112:8081/api/awarded/both", {
             headers: {
               Authorization: `Bearer ${token}`,
             },
