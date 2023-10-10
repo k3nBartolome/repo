43b6f27d94383ee99bfd26778e49be5a7db242b5
k3n Bartolome <53511874+k3nBartolome@users.noreply.github.com>
2023-10-10 13:14:12 +0800

    message

--- app/Http/Controllers/ClassStaffingController.php
+++ app/Http/Controllers/ClassStaffingController.php
@@ -151,7 +151,7 @@ class ClassStaffingController extends Controller
 ]);
     }
 
-    public function mpsWeek($dateRangeId = null, $monthNum = null, $siteId = null, $programId = null)
+    public function mpsWeek()
     {
         $staffing = DB::table('class_staffing')
     ->leftJoin('classes', 'class_staffing.classes_id', '=', 'classes.id')
@@ -174,24 +174,7 @@ class ClassStaffingController extends Controller
     )
     ->where('class_staffing.active_status', 1);
 
-        if ($dateRangeId !== null) {
-            $staffing->where('date_ranges.id', $dateRangeId);
-        }
-
-        if ($monthNum !== null) {
-            $staffing->orWhere('date_ranges.month_num', $monthNum);
-        }
-
-        if ($siteId !== null) {
-            $staffing->where('sites.id', $siteId);
-        }
-
-        if ($programId !== null) {
-            $staffing->where('programs.id', $programId);
-        }
-
         $staffing = $staffing->get();
-        $dateRangeIds = DB::table('date_ranges')->distinct()->pluck('date_id')->toArray();
         $groupedStaffing = $staffing->groupBy('date_range_id');
 
         $computedSums = [];
@@ -261,7 +244,141 @@ class ClassStaffingController extends Controller
         ]);
     }
 
+    /* public function mpsWeek()
+    {
+        // Query distinct site, program, and date_range IDs from the database
+        $sites = DB::table('sites')->pluck('id')->toArray();
+        $programs = DB::table('programs')->pluck('id')->toArray();
+        $dateRanges = DB::table('date_ranges')->pluck('id')->toArray();
+    
+        $allCombinations = [];
+    
+        foreach ($sites as $siteId) {
+            foreach ($programs as $programId) {
+                foreach ($dateRanges as $dateRangeId) {
+                    $allCombinations[] = [
+                        'site_id' => $siteId,
+                        'program_id' => $programId,
+                        'date_range_id' => $dateRangeId,
+                    ];
+                }
+            }
+        }
+    
+        $staffing = DB::table('class_staffing')
+            ->leftJoin('classes', 'class_staffing.classes_id', '=', 'classes.id')
+            ->leftJoin('date_ranges', 'classes.date_range_id', '=', 'date_ranges.id')
+            ->leftJoin('sites', 'classes.site_id', '=', 'sites.id')
+            ->leftJoin('programs', 'classes.program_id', '=', 'programs.id')
+            ->select(
+                'class_staffing.*',
+                'classes.*',
+                'sites.*',
+                'programs.*',
+                'date_ranges.*',
+                DB::raw('COALESCE(date_ranges.date_id, 0) as date_range_id'),
+                DB::raw('COALESCE(date_ranges.month_num, 0) as month_num'),
+                DB::raw('COALESCE(date_ranges.date_range, 0) as week_name'),
+                DB::raw('COALESCE(sites.site_id, 0) as site_id'),
+                DB::raw('COALESCE(programs.program_id, 0) as program_id'),
+                DB::raw('COALESCE(sites.name, 0) as site_name'),
+                DB::raw('COALESCE(programs.name, 0) as program_name'),
+            )
+            ->where('class_staffing.active_status', 1);
+    
+        $groupedData = $staffing->groupBy(['site_id', 'program_id', 'date_range_id']);
+    
+        $classesBySiteProgramDateRange = [];
+    
+        foreach ($allCombinations as $combination) {
+            $siteId = $combination['site_id'];
+            $programId = $combination['program_id'];
+            $dateRangeId = $combination['date_range_id'];
+    
+            if (isset($groupedData["$siteId_$programId_$dateRangeId"])) {
+                $classesBySiteProgramDateRange[] = [
+                    'site_id' => $siteId,
+                    'program_id' => $programId,
+                    'date_range_id' => $dateRangeId,
+                    'classes' => $groupedData["$siteId_$programId_$dateRangeId"]->toArray(),
+                ];
+            } else {
+                $classesBySiteProgramDateRange[] = [
+                    'site_id' => $siteId,
+                    'program_id' => $programId,
+                    'date_range_id' => $dateRangeId,
+                    'classes' => [],
+                ];
+            }
+        }
+    
+        $computedSums = [];
 
+        $grandTotals = [
+            'total_target' => 0,
+            'internal' => 0,
+            'external' => 0,
+            'total' => 0,
+            'cap_starts' => 0,
+            'day_1' => 0,
+            'day_2' => 0,
+            'day_3' => 0,
+            'day_4' => 0,
+            'day_5' => 0,
+            'filled' => 0,
+            'open' => 0,
+            'classes' => 0,
+        ];
+
+        foreach ($groupedStaffing as $monthNum => $group) {
+            $computedSums[$monthNum] = [
+                'month' => $group->first()->month,
+                'week_name' => $group->first()->week_name,
+                'program_name' => $group->first()->site_name,
+                'site_name' => $group->first()->program_name,
+                'total_target' => $group->sum('total_target'),
+                'internal' => $group->sum('show_ups_internal'),
+                'external' => $group->sum('show_ups_external'),
+                'total' => $group->sum('show_ups_total'),
+                'cap_starts' => $group->sum('cap_starts'),
+                'day_1' => $group->sum('day_1'),
+                'day_2' => $group->sum('day_2'),
+                'day_3' => $group->sum('day_3'),
+                'day_4' => $group->sum('day_4'),
+                'day_5' => $group->sum('day_5'),
+                'filled' => $group->sum('open'),
+                'open' => $group->sum('filled'),
+                'classes' => $group->sum('classes_number'),
+            ];
+
+            foreach ($grandTotals as $key => $value) {
+                $grandTotals[$key] += $computedSums[$monthNum][$key];
+            }
+        }
+
+        $computedSums[] = [
+            'month' => 'Total',
+            'week_name' => '',
+            'total_target' => $grandTotals['total_target'],
+            'internal' => $grandTotals['internal'],
+            'external' => $grandTotals['external'],
+            'total' => $grandTotals['total'],
+            'cap_starts' => $grandTotals['cap_starts'],
+            'day_1' => $grandTotals['day_1'],
+            'day_2' => $grandTotals['day_2'],
+            'day_3' => $grandTotals['day_3'],
+            'day_4' => $grandTotals['day_4'],
+            'day_5' => $grandTotals['day_5'],
+            'filled' => $grandTotals['filled'],
+            'open' => $grandTotals['open'],
+            'classes' => $grandTotals['classes'],
+        ];
+    
+        return response()->json([
+            'mps' => $computedSums,
+            'classes_by_site_program_date_range' => $classesBySiteProgramDateRange,
+        ]);
+    } */
     /*     public function mps($dateRangeId = null, $monthNum = null, $siteId = null, $programId = null)
         {
             $staffing = DB::table('class_staffing')
